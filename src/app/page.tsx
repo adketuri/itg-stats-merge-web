abd315d7ef336222902f46b766a1650046351660
Andrew Keturi <adketuri@gmail.com>
2024-01-17 21:26:20 -0800

    Hook in local itg-stats-merge

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,16 @@
+"use client"
+
 import { Box } from '@chakra-ui/react'
+import { useEffect } from 'react'
+import { main } from "itg-stats-merge"
 
 export default function Home() {
+  useEffect(() => {
+    console.log("hello")
+    main()
+  }, [])
   return (
     <Box>Hello</Box>
   )
 }
+
